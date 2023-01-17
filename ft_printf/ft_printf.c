1fd66983fe1b007df15c89be5ba285fb289d2e84
Lars Peeters <lpeeters@a2-r1-p8.s19.be>
2023-01-17 19:19:57 +0100

    testers + rmgit + modified ft_printf, now completed

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: lpeeters <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 15:51:27 by lpeeters          #+#    #+#             */
-/*   Updated: 2023/01/17 16:51:22 by lpeeters         ###   ########.fr       */
+/*   Updated: 2023/01/17 18:01:42 by lpeeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_pustr(char *str)
+int	ft_putstr(char *str)
 {
 	int	i;
 
