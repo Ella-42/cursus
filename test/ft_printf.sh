55a2802f3967f5beb4aa852376efcb8190dd665a
Lars Peeters <lpeeters@a2-r1-p8.s19.be>
2023-01-30 17:12:19 +0100

    deleted unnecessary files and tests

--- test/ft_printf.sh
+++ test/ft_printf.sh
@@ -1,1 +1,1 @@
-git clone git@vogsphere-v2.s19.be:vogsphere/intra-uuid-baa11054-6e3b-4732-a977-d00d2dd4a7cf-4630291-lpeeters ft_printf && rm -rf ft_printf/.git
+git clone git@vogsphere-v2.s19.be:vogsphere/intra-uuid-1eac6c8b-b179-46da-8d67-52305d865d41-4646317-lpeeters ft_printf && rm -rf ft_printf/.git
