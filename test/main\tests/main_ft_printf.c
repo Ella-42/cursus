8d5baca3c0f62f48ca0818ed61f7ac128d479278
Lars Peeters <lpeeters@a2-r1-p8.s19.be>
2023-01-31 15:33:21 +0100

    main folder

---
+++ test/main\tests/main_ft_printf.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpeeters <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/31 13:06:14 by lpeeters          #+#    #+#             */
+/*   Updated: 2023/01/31 15:16:01 by lpeeters         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <stdio.h>
+#include <limits.h>
+
+int	main(void)
+{
+/*	printf(" %p ", (void *)-1);
+	printf("\n");
+        printf(" %p ", (void *)1);
+	printf("\n");
+        printf(" %p ", (void *)15);
+	printf("\n");
+        printf(" %p ", (void *)16);
+	printf("\n");
+        printf(" %p ", (void *)17);
+	printf("\n");
+        printf(" %p %p ", (void *)LONG_MIN, (void *)LONG_MAX);
+	printf("\n");
+        printf(" %p %p ", (void *)INT_MIN, (void *)INT_MAX);
+	printf("\n");
+        printf(" %p %p ", (void *)ULONG_MAX, (void *)-ULONG_MAX);
+	printf("\n");
+        printf(" %p %p ", (void *)0, (void *)0);
+	printf("\n");
+	ft_printf(" %p ", (void *)-1);
+	printf("\n");
+        ft_printf(" %p ", (void *)1);
+	printf("\n");
+        ft_printf(" %p ", (void *)15);
+	printf("\n");
+        ft_printf(" %p ", (void *)16);
+	printf("\n");
+        ft_printf(" %p ", (void *)17);
+	printf("\n");
+        ft_printf(" %p %p ", (void *)LONG_MIN, (void *)LONG_MAX);
+	printf("\n");
+        ft_printf(" %p %p ", (void *)INT_MIN, (void *)INT_MAX);
+	printf("\n");
+        ft_printf(" %p %p ", (void *)ULONG_MAX, (void *)-ULONG_MAX);
+	printf("\n");
+        ft_printf(" %p %p ", (void *)0, (void *)0);
+	printf("\n");
+*/
+/*	printf("%-16p\n", NULL);
+	ft_printf("%-16p\n", NULL);
+*/
+}
