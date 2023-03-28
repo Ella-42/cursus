a30263c0151766f618912a1bd390dd8df0fbb113
LarsCampus19 <Larsp.jobs@gmail.com>
2023-03-28 17:58:45 +0200

    auto_push

--- tools/rm.sh
+++ tools/rm.sh
@@ -1,1 +1,1 @@
-find . \( -name "*.gch" -o -name "a.out" -o -name "*.o" \) -exec rm -rf {} \; -print
+find . \( -name "*.gch" -o -name "a.out" -o -name "*.o" -o -name "*.swp" \) -exec rm -rf {} \; -print
