c6f2e534a5915004b71ce2fb400e86fb01e8a1fd
LarsCampus19 <Larsp.jobs@gmail.com>
2023-03-31 23:19:10 +0200

    auto_push

--- push_swap/algorithm/master_algorithm.c
+++ push_swap/algorithm/master_algorithm.c
@@ -6,7 +6,7 @@
 /*   By: lpeeters <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 11:17:48 by lpeeters          #+#    #+#             */
-/*   Updated: 2023/03/29 14:31:31 by lpeeters         ###   ########.fr       */
+/*   Updated: 2023/03/31 23:18:52 by lpeeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,29 @@
 
 void	index_stack(t_node **stack)
 {
+	int		val;
 	int		min_val;
-	int		max_val;
-	int		len;
-	t_node	*current;
+	t_node	*index;
 
+	val = 0;
 	min_val = (*stack)->val;
-	max_val = (*stack)->val;
-	len = 0;
-	current = *stack;
-	while (current != NULL)
+	index = *stack;
+	while (index != NULL)
 	{
-		if (current->val < min_val)
-			min_val = current->val;
-		else if (current->val > max_val)
-			max_val = current->val;
-		len++;
-		current = current->next;
+		if (index->val < min_val)
+			min_val = index->val;
+		index = index->next;
 	}
-	ft_printf("minimum value: %d\n", min_val);
-	ft_printf("maximum value: %d\n", max_val);
-	ft_printf("length: %d\n", len);
+	index = *stack;
+	while (index != NULL)
+	{
+		if (index->val == min_val)
+		{
+			index->val = val;
+			val++;
+		}
+		index = index->next;
+	}
 }
 
 /*check if list is sorted*/
@@ -69,7 +71,7 @@ void	master_algorithm(int ac, char **av)
 	stack_b = ft_llb();
 	print_list(&stack_a);
 	index_stack(&stack_a);
-	print_list(&stack_b);
-	index_stack(&stack_b);
+	print_list(&stack_a);
 	free_ll(stack_a);
+	free_ll(stack_b);
 }
