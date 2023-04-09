56ef66debfbbebe82676c2b625e4508549e8f376
ella <ellakazam.h@gmail.com>
2023-04-09 18:14:29 +0200

    auto_push

---
+++ push_swap/algorithm/algorithm.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algorithm.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpeeters <lpeeters@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/09 17:25:49 by lpeeters          #+#    #+#             */
+/*   Updated: 2023/04/09 17:25:54 by lpeeters         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+/*algorithm for any amount of numbers bigger than 5*/
+
+void	algo(t_node **stack_a)
+{
+	t_node	*stack_b;
+
+	stack_b = ft_llb();
+}
