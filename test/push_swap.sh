894f5019f37dbb064810c5a958dd0e5052c5878f
LarsCampus19 <Larsp.jobs@gmail.com>
2023-05-05 22:13:10 +0200

    auto_push

--- test/push_swap.sh
+++ test/push_swap.sh
@@ -1,1 +1,1 @@
-git submodule add git@vogsphere-v2.s19.be:vogsphere/intra-uuid-f3a1ae3c-22e3-46a2-b2f2-57a28e00cbd6-4736000-lpeeters push_swap
+git submodule add git@github.com:LarsCampus19/push_swap.git
