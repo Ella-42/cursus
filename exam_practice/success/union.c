bbc923f441f88814d6656cb9dd9063d57eefe2cd
LarsCampus19 <Larsp.jobs@gmail.com>
2023-05-22 23:23:16 +0200

    completed ft_union(auto_push)

--- exam_practice/success/union.c
+++ exam_practice/success/union.c
@@ -5,40 +5,73 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lpeeters <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/22 19:14:12 by lpeeters          #+#    #+#             */
-/*   Updated: 2023/05/22 19:32:57 by lpeeters         ###   ########.fr       */
+/*   Created: 2023/05/22 21:36:13 by lpeeters          #+#    #+#             */
+/*   Updated: 2023/05/22 23:18:02 by lpeeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
-int	no_dup(int ac, char **av)
+int	ft_strlen(char *str)
 {
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	check_dup(char *written, char str)
+{
+	int	i;
+
+	i = 0;
+	while (written[i])
+	{
+		if (written[i] == str)
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 int	main(int ac, char **av)
 {
 	int		i;
 	int		j;
+	char	*str;
 
-	i = 0;
-	j = 0;
 	if (ac == 3)
 	{
-		while (av[1][i] != '\0')
+		char	written[ft_strlen(av[1]) + ft_strlen(av[2]) + 1];
+		i = ft_strlen(av[1]);
+		j = 0;
+		str = av[1];
+		while (av[2][j])
 		{
-			while (av[2][j] != '\0')
+			str[i] = av[2][j];
+			i++;
+			j++;
+		}
+		str[i] = '\0';
+		i = 0;
+		j = 0;
+		write(1, &str[i], 1);
+		written[j] = str[i];
+		j = 1;
+		while (str[i])
+		{
+			if (check_dup(written, str[i]))
 			{
-				if (no_dup(ac, av))
-				{
-					write(1, &av[1][i], 1);
-					j++;
-				}
-				else
-					j++;
+				write(1, &str[i], 1);
+				written[j] = str[i];
+				j++;
 			}
 			i++;
 		}
+		written[j] = '\0';
 	}
 	write(1, "\n", 1);
 }
