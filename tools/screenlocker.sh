143a2bb2328838bb93bb429069fa74992f537be5
LarsCampus19 <Larsp.jobs@gmail.com>
2023-06-29 19:55:29 +0200

    updated screenlocker script to also include the startup script when re-logging in(auto_push)

--- tools/screenlocker.sh
+++ tools/screenlocker.sh
@@ -5,7 +5,7 @@
 /nfs/homes/lpeeters/.local/bin/xdotool search --class "Terminal" windowkill %@
 
 # Command to close Firefox
-killall firefox
+pkill firefox
 
 # Wait for terminals and Firefox to close
 sleep 3
@@ -18,10 +18,14 @@ sleep 10
 
 # Turn up the brightness
 i=0;
-while ((i <= 25))
+while ((i < 35))
 do
 	sleep 0.1
 	/nfs/homes/lpeeters/.local/bin/xdotool keydown XF86MonBrightnessUp
+	sleep 0.1
 	/nfs/homes/lpeeters/.local/bin/xdotool keyup XF86MonBrightnessUp
 	((i++))
 done
+
+# Run the startup script
+/nfs/homes/lpeeters/.local/bin/xdotool key control+alt+t
