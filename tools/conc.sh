386ed0db35f73a390b47d01f4c27452fce7ea450
LarsCampus19 <Larsp.jobs@gmail.com>
2023-10-06 00:00:59 +0200

    reworked concatenator(auto_push)

--- tools/conc.sh
+++ tools/conc.sh
@@ -6,7 +6,7 @@ read pattern
 printf '\033[2J\033[H'
 
 find . -name "$pattern" | while read file; do
-  echo -e "\n==================== $file ====================\n"
+  echo "\n==================== $file ====================\n"
   tail -n +0 "$file"
 done > ~/log
 
