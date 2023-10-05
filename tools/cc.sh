e0d875c065ed016c11ecf0af58f351a144e502cd
LarsCampus19 <Larsp.jobs@gmail.com>
2023-10-06 00:51:18 +0200

    merged concatenators into one, smarter concatenator(auto_push)

--- tools/cc.sh
+++ tools/cc.sh
@@ -1,13 +1,28 @@
-#!/bin/bash
+#!/bin/zsh
 
-echo "Enter the search pattern:"
+# Ask the user what files they want to see
+echo "Enter a search pattern:"
 read pattern
 
-printf '\033[2J\033[H'
+# Clear the screen
+printf "\033[2J\033[H"
 
+# Print contents of all found files that match the search pattern to the log file
 find . -name "$pattern" | while read file; do
+  
+  # Print where this file was found
   echo -e "\n==================== $file ====================\n"
-  tail -n +15 "$file"
+
+  # Fetch the first line of the file
+  first_line=$(head -n 1 "$file")
+
+  # Compare the first line to the first line of the 42 header. If these match, remove it, else, do not
+  if [[ "$first_line" == "/* ************************************************************************** */" ]]; then
+    tail -n +15 "$file"
+  else
+    tail -n +3 "$file"
+  fi
 done > ~/log
 
+# Open the result in vim
 vim ~/log
