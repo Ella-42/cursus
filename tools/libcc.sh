00fa312488c7cbdb05764011f815810cb508b9a9
LarsCampus19 <Larsp.jobs@gmail.com>
2023-10-06 00:55:44 +0200

    added concatenator which skips libft(auto_push)

---
+++ tools/libcc.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+# Ask the user what files they want to see
+echo "Enter a search pattern:"
+read pattern
+
+# Clear the screen
+printf "\033[2J\033[H"
+
+# Print contents of all found files that match the search pattern to the log file
+find . -name "$pattern" -not -path "*libft*" | while read file; do
+  
+  # Print where this file was found
+  echo -e "\n==================== $file ====================\n"
+
+  # Fetch the first line of the file
+  first_line=$(head -n 1 "$file")
+
+  # Compare the first line to the first line of the 42 header. If these match, remove it, else, do not
+  if [[ "$first_line" == "/* ************************************************************************** */" ]]; then
+    tail -n +15 "$file"
+  else
+    tail -n +3 "$file"
+  fi
+done > ~/log
+
+# Open the result in vim
+vim ~/log
