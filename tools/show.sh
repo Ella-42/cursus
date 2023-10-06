3980533aff4e1483ab9e393317e771c8073e8476
LarsCampus19 <Larsp.jobs@gmail.com>
2023-10-06 18:20:13 +0200

    added tool to open files across branches(auto_push)

---
+++ tools/show.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+# Check if an argument was provided, if not, throw an error
+if ! [ $# -gt 0 ]; then
+	echo "Yeah... but which file though?"
+
+# Else, run the script
+else
+
+	# Fetch the requested file from the branch and store it
+	output=$(git show wmarien:$1)
+	
+	# Check if there was no error trying to fetch the file
+	if [ $? -eq 0 ]; then
+		
+		# Send the output to the log file
+		echo "$output" > ~/log
+
+		# Open the log file in Vim
+		vim ~/log
+	fi
+
+fi
+
+#Simple script to open files in Vim across branches
