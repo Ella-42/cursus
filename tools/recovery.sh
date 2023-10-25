22d6848ab7894eda18779174d9d6183d23308392
ella <ellakazam.h@gmail.com>
2023-10-25 21:46:20 +0200

    updated recovery git(auto_push)

--- tools/recovery.sh
+++ tools/recovery.sh
@@ -3,9 +3,9 @@
 # Script to recover from the backup repo
 
 # Copy over files and folders
-cp ~/recovery/.vimrc ~/
-cp ~/recovery/.zshrc ~/
-cp -r ~/recovery/view ~/.vim/
+cp ~/backup/.vimrc ~/
+cp ~/backup/.zshrc ~/
+cp -r ~/backup/view ~/.vim/
 
 # Run the custom Vim update script
 sh ~/Documents/github/tools/update_custom_vimconfig.sh
