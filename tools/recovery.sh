26f44cf6283ca9ee8f2c47955d20068a10d84bae
LarsCampus19 <Larsp.jobs@gmail.com>
2023-10-25 21:11:21 +0200

    updated minishell(auto_push)

--- tools/recovery.sh
+++ tools/recovery.sh
@@ -3,7 +3,9 @@
 # Script to recover from the backup repo
 
 # Copy over files and folders
-cp ~/recovery/.cvimrc ~/
 cp ~/recovery/.vimrc ~/
 cp ~/recovery/.zshrc ~/
 cp -r ~/recovery/view ~/.vim/
+
+# Run the custom Vim update script
+sh ~/Documents/github/tools/update_custom_vimconfig.sh
