df971bf6e78e290476d72ec93921d7470a424ea1
ella <ellakazam.h@gmail.com>
2024-01-25 17:27:52 +0100

    made simple backup script to add logic for different environments(auto_push)

---
+++ tools/backup.sh
@@ -0,0 +1,30 @@
+# Backup script
+
+# If we're in the windows 11 environment on wsl2, save backup in appropriate folder
+if [ -f ~/.env_w11 ]; then
+	cp -r ~/.config/terminator/ ~/backup/Windows11_WSL2/;
+	cp -r ~/.config/nvim/init.vim ~/backup/Windows11_WSL2/;
+	cp -r ~/.zshrc ~/backup/Windows11_WSL2/;
+	dir=$(pwd);
+	echo '=========================Backup=========================';
+	cd ~/backup/;
+	git add .;
+	git commit -m "Automatic backup";
+	git push;
+	echo '========================================================';
+	cd $dir;
+# Else, just run the script like normal
+else
+	cp -r ~/.config/terminator/ ~/backup/;
+	cp -r ~/.vimrc ~/backup/;
+	cp -r ~/.zshrc ~/backup/;
+	cp -r ~/.vim/view/ ~/backup/;
+	dir=$(pwd);
+	echo '=========================Backup=========================';
+	cd ~/backup/;
+	git add .;
+	git commit -m "Automatic backup";
+	git push;
+	echo '========================================================';
+	cd $dir;
+fi
