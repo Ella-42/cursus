d75b04b9f459d3b69258e1eb9c5d4259efde5f8c
Ella-42 <Ellakazam.h@gmail.com>
2024-04-29 17:42:16 +0200

    updated new norm script to use an entirely different method because previous version was way too slow(auto push :heart:)

--- tools/norm.sh
+++ tools/norm.sh
@@ -4,19 +4,5 @@
 # Simple script to only show Norminette errors and skip files which are OK
 
 
-# Move the cursor up one line
-up="\033[A"
-
-# Erase the current line on the cursor
-clear="\033[K"
-
-# Fetch all C and header files
-find . \( -name "*.c" -o -name "*.h" \) -not -path "*mlx*" | while read file;
-do
-	# Run Norminette on each file
-	norminette $file
-	# If the file was OK, remove the line
-	if [ "$?" -eq "0" ]; then
-		printf $up$clear;
-	fi
-done
+# Fetch all C and header files, run Norminette on each, only printing lines that were not OK
+find . \( -name "*.c" -o -name "*.h" \) -not -path "*mlx*" -print0 | xargs -0 norminette | grep -v 'OK!$'
