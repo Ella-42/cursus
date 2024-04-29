7bd282eba7cf6b2268a2193fc333f476ab5e8044
Ella-42 <Ellakazam.h@gmail.com>
2024-04-29 18:15:53 +0200

    updated so_long, updated norm script to print something in case no errors were found and to return logically

--- tools/norm.sh
+++ tools/norm.sh
@@ -6,3 +6,12 @@
 
 # Fetch all C and header files, run Norminette on each, only printing lines that were not OK
 find . \( -name "*.c" -o -name "*.h" \) -not -path "*mlx*" -print0 | xargs -0 norminette | grep -v 'OK!$'
+
+# If command failed due to having no lines leftover because of having no errors
+if [ $? -eq 1 ]; then
+	# inform user and script will end with success
+	echo "No Norminette errors";
+else
+	# else exit with failure to reflect errors
+	exit 1;
+fi
