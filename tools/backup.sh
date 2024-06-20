fbedaf5eb5093c643ab93cdb751d1a5f011d081e
Ella-42 <EllaP.jobs@gmail.com>
2024-06-20 10:56:31 +0200

    added -u flag for cp command to not do any unnecessary operations(auto push :heart:)

--- tools/backup.sh
+++ tools/backup.sh
@@ -4,9 +4,9 @@
 
 # If we're in the windows 11 environment on wsl2, save backup in appropriate folder
 if [ -f ~/.env_w11 ]; then
-	cp -r ~/.config/terminator/ ~/backup/Windows11_WSL2/;
-	cp -r ~/.config/nvim/init.vim ~/backup/Windows11_WSL2/;
-	cp -r ~/.zshrc ~/backup/Windows11_WSL2/;
+	cp -ru ~/.config/terminator/ ~/backup/Windows11_WSL2/;
+	cp -ru ~/.config/nvim/init.vim ~/backup/Windows11_WSL2/;
+	cp -ru ~/.zshrc ~/backup/Windows11_WSL2/;
 	dir=$(pwd);
 	echo '=========================Backup=========================';
 	cd ~/backup/;
@@ -18,13 +18,13 @@ if [ -f ~/.env_w11 ]; then
 
 # Else, just run the script like normal
 else
-	cp -r ~/.config/terminator/ ~/backup/;
-	cp -r ~/.vim/view/ ~/backup/;
-	cp ~/.config/nvim/init.vim ~/backup/;
-	cp ~/.vimrc ~/backup/;
-	cp ~/.zshrc ~/backup/;
-	cp ~/.valgrindrc ~/backup/;
-	cp ~/.gdbinit ~/backup/;
+	cp -ru ~/.config/terminator/ ~/backup/;
+	cp -ru ~/.vim/view/ ~/backup/;
+	cp -u ~/.config/nvim/init.vim ~/backup/;
+	cp -u ~/.vimrc ~/backup/;
+	cp -u ~/.zshrc ~/backup/;
+	cp -u ~/.valgrindrc ~/backup/;
+	cp -u ~/.gdbinit ~/backup/;
 	dir=$(pwd);
 	echo '=========================Backup=========================';
 	cd ~/backup/;
